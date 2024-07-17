bd8514445fbbfbc2e7834ba9c3847652fb07f1b2
LMCuber <leo.bozkir@outlook.com>
2024-07-17 19:04:56 +0200

    force push

--- main.py
+++ main.py
@@ -1,26 +1,59 @@
 #!/usr/bin/env python3
 
 import pygame
-from sys import exit
 from src.engine import *
+from src.player import *
+import sys
+from pathlib import Path
+import itertools
+
 
 pygame.init()
 
+clock = pygame.time.Clock()
+game = Game()
+player = Player()
+
+tiles = [
+    pygame.transform.scale_by(pygame.image.load(Path("assets", "empty.png")), R),
+    pygame.transform.scale_by(pygame.image.load(Path("assets", "tile.png")), R),
+]
+
+class World:
+    def __init__(self):
+        self.data = dict.fromkeys(itertools.product(range(10), range(10)), 1)
+
+
+world = World()
+
 
 # test_font = pygame.font.Font("path/font.ttf")
-
-
 def main():
-    clock = pygame.time.Clock()
-    display = pygame.display.set_mode((1280, 720))
-
-    game = Game()
-
     while game.running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
-                exit()
+                sys.exit()
+                
+        display.fill(LIGHT_GRAY)
+
+        for pos, tile in world.data.items():
+            x, y = pos
+            # minimap
+            mm_x = x * MMS
+            mm_y = y * MMS
+            pygame.draw.rect(display, (106, 190, 48), (mm_x, mm_y, MMS, MMS))
+            pygame.draw.rect(display, WHITE, (mm_x, mm_y, MMS, MMS), 1)
+            blit_x = ORIGIN[0] + x * HS - y * HS
+            blit_y = ORIGIN[1] + x * QS + y * QS
+            # map
+            display.blit(tiles[tile], (blit_x, blit_y))
+
+        player.update()
 
         pygame.display.update()
         clock.tick(game.target_fps)
+
+
+if __name__ == "__main__":
+    main()
--- src/engine.py
+++ src/engine.py
@@ -1,3 +1,21 @@
+import pygame
+
+
+BLACK = (0, 0, 0)
+LIGHT_GRAY = (200, 200, 200)
+WHITE = (255, 255, 255)
+
+
+WIDTH, HEIGHT = 1280, 720
+MMS = 15
+R = 3
+S = 31 * R
+HS = 15 * R
+QS = 8 * R
+ORIGIN = (500, 30)
+
+display = pygame.display.set_mode((WIDTH, HEIGHT))
+
 class Game:
     def __init__(self) -> None:
         self.running = True
